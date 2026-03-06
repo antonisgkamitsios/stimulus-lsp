446ed5a692f31552c5833e4d9b4aabbd5b69c06e
Antonios Gkamitsios <angam97@gmail.com>
2026-03-06 18:24:58 +0200

    Add informative message when lang configuration changes

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,5 +1,5 @@
 import * as path from 'path';
-import { ExtensionContext, window, workspace, WorkspaceConfiguration } from 'vscode';
+import { commands, ExtensionContext, window, workspace, WorkspaceConfiguration } from 'vscode';
 
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
 import { StimulusSettings, defaultSettings, LSP_ID } from 'shared';
@@ -11,6 +11,10 @@ function getSetting<K extends keyof StimulusSettings>(
   key: K,
 ): StimulusSettings[K] {
   return configuration.get(key) ?? defaultSettings[key];
+}
+
+function getPrefixedSettingKey(string: keyof StimulusSettings): string {
+  return `${LSP_ID}.${string}`;
 }
 
 export function activate(context: ExtensionContext) {
@@ -34,10 +38,21 @@ export function activate(context: ExtensionContext) {
   const activationLanguages = getSetting(stimulusConfig, 'activationLanguages');
 
   const clientOptions: LanguageClientOptions = {
-    // Register the server for HTML documents
     documentSelector: activationLanguages.map((lang) => ({ scheme: 'file', language: lang })),
     outputChannel: outputChannel,
   };
+
+  workspace.onDidChangeConfiguration((event) => {
+    if (event.affectsConfiguration(getPrefixedSettingKey('activationLanguages'))) {
+      window
+        .showInformationMessage('Stimulus LSP: Reload window to apply changes.', 'Reload Window')
+        .then((selection) => {
+          if (selection === 'Reload Window') {
+            commands.executeCommand('workbench.action.reloadWindow');
+          }
+        });
+    }
+  });
 
   // Create the language client and start the client.
   client = new LanguageClient(LSP_ID, 'Stimulus LSP', serverOptions, clientOptions);
