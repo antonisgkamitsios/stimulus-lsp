616fbe42f1e66cb3c07d5eff1777ae0b4405a0cc
Antonios Gkamitsios <angam97@gmail.com>
2026-03-22 11:25:42 +0200

    Make more changes to the build

--- server/esbuild.js
+++ server/esbuild.js
@@ -1,4 +1,9 @@
 import { build } from 'esbuild';
+import { cpSync } from 'fs';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 build({
   entryPoints: ['src/server.ts'],
@@ -8,4 +13,8 @@ build({
   format: 'cjs',
   platform: 'node',
   sourcemap: true,
+}).then(() => {
+  const src = resolve(__dirname, '../node_modules/vscode-html-languageservice');
+  const dest = resolve(__dirname, 'out/node_modules/vscode-html-languageservice');
+  cpSync(src, dest, { recursive: true });
 });
